bb5e89001d15db5583292b4450966bb8bb726b29
_raouf <raouffassih@gmail.com>
2025-03-30 23:31:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//this file is kinda useless
+//by raouf ;)
+let navLinks = document.getElementById("n-links")
+function showNav(){
+    navLinks.style.display = "flex"
+}
+function hideNav(){
+    navLinks.style.display = "none"
+}
